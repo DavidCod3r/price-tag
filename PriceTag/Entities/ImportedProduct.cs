dfd072a85912f69487d0c0f841ec4ed932e44fbf
David Almeida <davidalmeidadrive@gmail.com>
2020-05-27 22:50:43 -0300

    classes

---
+++ PriceTag/Entities/ImportedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceTag.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag();
+        }
+    }
+}
---
+++ PriceTag/Entities/UsedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceTag.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct(string name, double price, DateTime monufactureDate) : base(name, price)
+        {
+            ManufactureDate = ManufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag();
+        }
+    }
+}
