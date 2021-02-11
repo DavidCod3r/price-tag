16c4fdd9a306fd19fd5fbc4720f6f6f4abd7c266
David Almeida <60515500+DavidCod3r@users.noreply.github.com>
2021-02-11 12:24:28 -0300

    LT refact

--- PriceTag/Program.cs
+++ PriceTag/Program.cs
@@ -9,34 +9,27 @@ namespace PriceTag
     {
         static void Main(string[] args)
         {
+
             List<Product> list = new List<Product>();
 
             Console.Write("Enter the number of products: ");
             int n = int.Parse(Console.ReadLine());
 
-            for(int i=1; i <=n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine($"Product #{i} data: ");
-                Console.Write("Commom, used or imported (c/u/i): ");
+                Console.Write("Common, used, or imported (c/u/i): ");
                 char ch = char.Parse(Console.ReadLine());
-                if(ch == 'i')
+                if (ch == 'C' || ch == 'c')
                 {
                     Console.Write("Name: ");
                     string name = Console.ReadLine();
                     Console.Write("Price: ");
                     double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    Console.Write("Customs fee: ");
-                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new ImportedProduct(name, price, customsFee));
-                }
-                else if (ch == 'c')
-                {
-                    Console.Write("Name: ");
-                    string name = Console.ReadLine();
-                    Console.Write("Price: ");
-                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
                     list.Add(new Product(name, price));
-                }else
+                }
+                else if (ch == 'U' || ch == 'u')
                 {
                     Console.Write("Name: ");
                     string name = Console.ReadLine();
@@ -44,8 +37,25 @@ namespace PriceTag
                     double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     Console.Write("Manufacture date (DD/MM/YYYY): ");
                     DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
                     list.Add(new UsedProduct(name, price, manufactureDate));
                 }
+                else if (ch == 'I' || ch == 'i')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else
+                {
+                    Console.WriteLine("Opção invalida!");
+                }
+                Console.WriteLine();
             }
 
             Console.WriteLine();
